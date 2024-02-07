476e9898bc72e3c8ae3d1203bae137daa6ea3acd
LucasLadreyt <lucasladreyt@posteo.de>
2024-02-07 14:32:58 +0200

    chore: kiwi: modularized default config

---
+++ modules/nginx.nix
@@ -0,0 +1,23 @@
+{ ... }:
+let secrets = import ../secrets/git-crypt.nix;
+in
+{
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = secrets.ldryt.email;
+  };
+  services.nginx = {
+    enable = true;
+    recommendedTlsSettings = true;
+    recommendedOptimisation = true;
+    recommendedGzipSettings = true;
+    sslCiphers = "AES256+EECDH:AES256+EDH:!aNULL";
+    sslProtocols = "TLSv1.3";
+    appendHttpConfig = ''
+      map $scheme $hsts_header {
+          https   "max-age=31536000; includeSubdomains; preload";
+      }
+      add_header Strict-Transport-Security $hsts_header;
+    '';
+  };
+}
---
+++ modules/podman.nix
@@ -0,0 +1,5 @@
+{ ... }:
+{
+  virtualisation.podman.enable = true;
+  virtualisation.oci-containers.backend = "podman";
+}
