184a31ce5f49be4327afceaf2907b3c7269e9e82
Lucas Ladreyt <lucasladreyt@posteo.de>
2024-02-12 13:41:48 +0200

    kiwi: glouton samba share caching options

--- hosts/kiwi/default.nix
+++ hosts/kiwi/default.nix
@@ -32,7 +32,7 @@ in {
   fileSystems."/mnt/glouton" = {
     device = hidden.kiwi.smb.glouton.shareName;
     fsType = "cifs";
-    options = ["x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,credentials=${config.sops.secrets."system/smb/glouton/credentials".path},uid=1000,gid=100"];
+    options = ["x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,credentials=${config.sops.secrets."system/smb/glouton/credentials".path},uid=1000,gid=100,cache=loose,fsc"];
   };
 
   virtualisation.vmVariant = {
