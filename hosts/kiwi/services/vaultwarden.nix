b5fe5b27c29cdc37051e53f2bc93257e697df829
Lucas Ladreyt <lucasladreyt@posteo.de>
2024-02-19 16:16:10 +0200

    kiwi: vaultwarden: disabled backups

--- hosts/kiwi/services/vaultwarden.nix
+++ hosts/kiwi/services/vaultwarden.nix
@@ -1,20 +1,16 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 let
   hidden = import ../../../secrets/obfuscated.nix;
   vaultwardenSubdomain = "pass";
-  gloutonPath = "/mnt/glouton";
-  backupDirName = "backups-pool";
-  backupGroupName = "backupShare";
 in {
   services.vaultwarden = {
     enable = true;
     config = {
       DOMAIN = "https://${vaultwardenSubdomain}.${hidden.ldryt.host}";
-      SIGNUPS_ALLOWED = "false";
+      SIGNUPS_ALLOWED = "true";
       ROCKET_ADDRESS = "127.0.0.1";
       ROCKET_PORT = 44083;
     };
-    backupDir = "${gloutonPath}/${backupDirName}/vaultwarden";
   };
 
   services.nginx = {
@@ -29,20 +25,5 @@ in {
           }";
       };
     };
-  };
-
-  users.groups.${backupGroupName}.gid = 4441;
-  users.users.vaultwarden.extraGroups = [ "${backupGroupName}" ];
-  environment.systemPackages = [ pkgs.cifs-utils ];
-  fileSystems."${gloutonPath}/${backupDirName}" = {
-    device = hidden.kiwi.smb.glouton.${backupDirName}.shareName;
-    fsType = "cifs";
-    options = [
-      "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,credentials=${
-        config.sops.secrets."system/smb/glouton/${backupDirName}/credentials".path
-      },gid=${
-        toString config.users.groups.${backupGroupName}.gid
-      },file_mode=0660,dir_mode=0770,cache=loose,fsc,sfu"
-    ];
   };
 }
