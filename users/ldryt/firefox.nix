3e64af56c325b1098753973c11cb81e9cf9c5dab
Lucas Ladreyt <lucasladreyt@posteo.de>
2024-03-10 23:01:15 +0200

    ldryt: firefox: gnome theming

--- users/ldryt/firefox.nix
+++ users/ldryt/firefox.nix
@@ -1,6 +1,15 @@
 { ... }: {
-  programs.firefox.enable = true;
+  home.file."firefox-gnome-theme" = {
+    target = ".mozilla/firefox/default/chrome/firefox-gnome-theme";
+    source = (fetchTarball {
+      url =
+        "https://github.com/rafaelmardojai/firefox-gnome-theme/archive/refs/tags/v122.tar.gz";
+      sha256 = "0mack8i6splsywc5h0bdgh1njs4rm8fsi0lpvvwmbdqmjjlkz6a1";
+    });
+  };
+
   programs.firefox = {
+    enable = true;
     profiles.default = {
       #    extensions = with nur.repos.rycee.firefox-addons; [
       #      ublock-origin
@@ -34,7 +43,19 @@
         "browser.formfill.enable" = false;
         "signon.autofillForms" = false;
         "signon.rememberSignons" = false;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "svg.context-properties.content.enabled" = true;
+        # "gnomeTheme.hideSingleTab" = true;
+        # "gnomeTheme.bookmarksToolbarUnderTabs" = true;
+        # "gnomeTheme.normalWidthTabs" = false;
+        # "gnomeTheme.tabsAsHeaderbar" = false;
       };
+      userChrome = ''
+        @import "firefox-gnome-theme/userChrome.css";
+      '';
+      userContent = ''
+        @import "firefox-gnome-theme/userContent.css";
+      '';
     };
   };
 }
