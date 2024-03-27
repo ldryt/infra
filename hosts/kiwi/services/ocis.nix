ba01be79fdbdb4b80aed7316a1c9089bd5ebb33d
Lucas Ladreyt <lucasladreyt@posteo.de>
2024-03-27 19:41:55 +0200

    fix(hosts/kiwi): Removed systemd-automount for CIFS mount

--- hosts/kiwi/services/ocis.nix
+++ hosts/kiwi/services/ocis.nix
@@ -90,9 +90,9 @@
     device = vars.sensitive.services.minio.smbShare;
     fsType = "cifs";
     options = [
-      "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,credentials=${
+      "async,rw,auto,nofail,credentials=${
         config.sops.secrets."system/smb/glouton/minio-buckets/credentials".path
-      },uid=${toString config.users.users.colon.uid},cache=loose,fsc,sfu"
+      },uid=${toString config.users.users.colon.uid},cache=loose,fsc"
     ];
   };
 
