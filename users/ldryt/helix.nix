c42f761d2f6ee77134d3d43e69637e684dfc7fe9
Lucas Ladreyt <lucasladreyt@posteo.de>
2024-04-06 12:15:39 +0300

    feat(users/ldryt): Helix TeX support

--- users/ldryt/helix.nix
+++ users/ldryt/helix.nix
@@ -7,24 +7,49 @@
     extraPackages = with pkgs;
       with nodePackages;
       with python311Packages; [
-        vscode-langservers-extracted
-        gopls
-        gotools
-        typescript
-        typescript-language-server
-        marksman
-        nil
-        nixfmt
-        clang-tools
-        lldb
+        # TeX
+        texlab
+        texliveMedium
+
+        # Rust
         rust-analyzer
         rustfmt
+        clang-tools
+        lldb
+
+        # Nix
+        nil
+        nixfmt
+
+        # GO
+        gopls
+        gotools
+
+        # Typescript
+        typescript
+        typescript-language-server
+
+        # Bash
         bash-language-server
+
+        # Python
         black
         python-lsp-server
+
+        # YAML
         yaml-language-server
+
+        # TOML
         taplo-lsp
+
+        # Markdown
+        marksman
+
+        # HCL
         terraform-ls
+
+        # Misc
+        vscode-langservers-extracted
       ];
     settings = {
       editor = {
@@ -35,7 +60,27 @@
       theme = "gruvbox";
     };
     languages = {
+      language-server = {
+        texlab.config.texlab = {
+          build = {
+            onSave = true;
+            forwardSearchAfter = true;
+          };
+          forwardSearch = {
+            executable = "evince";
+            args = [ "%p" ];
+          };
+          chktex = {
+            onOpenAndSave = true;
+            onEdit = true;
+          };
+        };
+      };
       language = [
+        {
+          name = "latex";
+          auto-format = true;
+        }
         {
           name = "rust";
           formatter.command = "rustfmt";
