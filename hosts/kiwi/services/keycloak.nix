70069ef43e9c76a2e4061c36ba4cb8ee9fc63460
Lucas Ladreyt <lucasladreyt@posteo.de>
2024-04-07 15:09:04 +0300

    feat(hosts/kiwi): Keycloak reverse-proxy

--- hosts/kiwi/services/keycloak.nix
+++ hosts/kiwi/services/keycloak.nix
@@ -18,4 +18,19 @@
       passwordFile = config.sops.secrets."services/keycloak/db/password".path;
     };
   };
+
+  services.caddy.virtualHosts."${vars.services.keycloak.subdomain}.${vars.zone}".extraConfig = ''
+    header {
+      -Server
+      Strict-Transport-Security "max-age=31536000; includeSubDomains; preload"
+      X-Xss-Protection "1; mode=block"
+      X-Content-Type-Options "nosniff"
+      Permissions-Policy interest-cohort=()
+      Content-Security-Policy "upgrade-insecure-requests"
+      Referrer-Policy "strict-origin-when-cross-origin"
+      Cache-Control "public, max-age=15, must-revalidate"
+      Feature-Policy "accelerometer 'none'; ambient-light-sensor 'none'; autoplay 'self'; camera 'none'; encrypted-media 'none'; fullscreen 'self'; geolocation 'none'; gyroscope 'none';       magnetometer 'none'; microphone 'none'; midi 'none'; payment 'none'; picture-in-picture *; speaker 'none'; sync-xhr 'none'; usb 'none'; vr 'none'"
+    }
+    reverse_proxy http://${config.services.keycloak.settings.http-host}:${toString config.services.keycloak.settings.http-port}
+  '';
 }
