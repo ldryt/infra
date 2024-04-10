c1f6590b3b47b9bf8907696846cb422c81986b62
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-04-10 13:32:02 +0300

    feat(users/ldryt): Change default git email

--- users/ldryt/git.nix
+++ users/ldryt/git.nix
@@ -6,6 +6,6 @@
   programs.git = {
     enable = true;
     userName = "${vars.sensitive.users.ldryt.name} ${vars.sensitive.users.ldryt.surname}";
-    userEmail = vars.sensitive.users.ldryt.email;
+    userEmail = "git@ldryt.anonaddy.me";
   };
 }
