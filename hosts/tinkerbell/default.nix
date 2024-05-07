2e1d2537c995dbd9bfa6e45619a9276891cf6548
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-05-07 20:48:18 +0300

    chore(hosts/tinkerbell): Set a fixed timezone

--- hosts/tinkerbell/default.nix
+++ hosts/tinkerbell/default.nix
@@ -84,7 +84,7 @@
     [system_default_sect]
     CipherString = Default:@SECLEVEL=0
   '';
-  time.timeZone = lib.mkForce null; # allow TZ to be set by user
+  time.timeZone = "Europe/Vilnius";
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
     LC_TIME = "fr_FR.UTF-8";
