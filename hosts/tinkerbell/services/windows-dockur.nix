559451aa38be55d913ef721c4bf694925b741700
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-05-13 15:06:29 +0300

    refactor(hosts/tinkerbell): Increase windows VM specs

--- hosts/tinkerbell/services/windows-dockur.nix
+++ hosts/tinkerbell/services/windows-dockur.nix
@@ -1,7 +1,3 @@
-# https://github.com/dockur/windows/issues/261
-
-# RDP access: nix run nixpkgs#rdesktop -- -u docker 127.0.0.1:3389
-# WEB access: http://127.0.0.1:8006/
 { ... }:
 let
   winVersion = "ltsc10";
@@ -11,11 +7,11 @@ in
   virtualisation.oci-containers.containers."windows-${winVersion}" = {
     image = "ghcr.io/dockur/windows:2.10@sha256:8d5918162e2ecc5da08c611676ecf958f96b35ba7e45bdb2a0774641d94c07f0";
     user = "root:root";
-    autoStart = false; # start it when needed with `sudo systemctl start podman-windows.service`
+    autoStart = false;
     environment = {
       VERSION = winVersion;
-      RAM_SIZE = "4G";
-      CPU_CORES = "2";
+      RAM_SIZE = "6G";
+      CPU_CORES = "6";
       DISK_SIZE = "200G";
     };
     ports = [
