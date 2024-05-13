417507e71ae0c37c8dbac44410ede5f71e154877
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-05-13 15:06:55 +0300

    feat(users/ldryt): Script to launch tinkerbell's windows VM

--- users/ldryt/aliases.nix
+++ users/ldryt/aliases.nix
@@ -1,6 +1,18 @@
 { ... }:
 {
-  home.shellAliases = {
-    "windows" = "nix shell nixpkgs#freerdp -c wlfreerdp /u:docker /p: /v:127.0.0.1:3389 -encryption +clipboard /rfx /gfx:rfx /f /floatbar:sticky:off +gestures +fonts /bpp:32 /audio-mode:0 +aero +window-drag /size:120 &";
+  home.file."windowsScript" = {
+    target = "windows.sh";
+    text = ''
+      #!/usr/bin/env
+
+      sudo systemctl start podman-windows-ltsc10.service
+      sleep 10
+      nix shell nixpkgs#freerdp -c \
+        wlfreerdp /u:docker /p: /v:127.0.0.1:3389 \
+        -encryption +clipboard /rfx /gfx:rfx /f \
+        /floatbar:sticky:off +gestures +fonts /bpp:32 \
+        /audio-mode:0 +aero +window-drag /size:120
+      sudo systemctl stop podman-windows-ltsc10.service
+    '';
   };
 }
