9cae73b2ede11e263c1087910aef9056aaa9e54d
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-05-14 17:30:49 +0300

    chore(hosts/tinkerbell): windows-dockur: add rdp command as a comment

--- hosts/tinkerbell/services/windows-dockur.nix
+++ hosts/tinkerbell/services/windows-dockur.nix
@@ -1,3 +1,5 @@
+# nix shell nixpkgs#freerdp -c wlfreerdp /u:docker /p: /v:127.0.0.1:3389 -encryption +clipboard /rfx /gfx:rfx /f /floatbar:sticky:off +gestures +fonts /bpp:32 /audio-mode:0 +aero +window-drag /size:120
+
 { ... }:
 let
   winVersion = "ltsc10";
