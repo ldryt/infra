13cff0f7554d0d3be39c0501145ebc722b0f8f60
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-05-19 20:39:16 +0300

    fix(hosts/tinkerbell): Remove libvirtd

--- hosts/tinkerbell/default.nix
+++ hosts/tinkerbell/default.nix
@@ -101,21 +101,6 @@
   services.xserver.xkb.layout = "fr";
 
   virtualisation.virtualbox.host.enable = true;
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      runAsRoot = true;
-      swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [(pkgs.OVMF.override {
-          secureBoot = true;
-          tpmSupport = true;
-        }).fd];
-      };
-    };
-  };
 
   sops.secrets."users/ldryt/hashedPassword".neededForUsers = true;
   users = {
