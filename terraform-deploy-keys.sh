0e08c25f64d230a6aafc69ff15b47dd1baf6d128
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-06-01 21:15:25 +0300

    fix(terraform): Change script filename

--- terraform-deploy-keys.sh
+++ terraform-deploy-keys.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SECRETS_FILE="$(dirname "$SCRIPT_DIR")/hosts/$SERVER_NAME/secrets.yaml"
 
 umask 0177
-cp "$KEYRING_DIR/sops_age_$SERVER_NAME.key" ./var/lib/sops
+cp "$KEYRING_PATH/sops_age_$SERVER_NAME.key" ./var/lib/sops
 umask 0022
 
 for keyname in ssh_host_rsa_key ssh_host_rsa_key.pub ssh_host_ed25519_key ssh_host_ed25519_key.pub; do
