8854968abefd8162d8ea2170062257d4035a7b68
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-06-02 12:45:45 +0300

    fix(terraform): Fix sops file path

--- terraform-deploy-keys.sh
+++ terraform-deploy-keys.sh
@@ -3,7 +3,7 @@
 mkdir -p ./etc/ssh ./var/lib/sops
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-SECRETS_FILE="$(dirname "$SCRIPT_DIR")/hosts/$SERVER_NAME/secrets.yaml"
+SECRETS_FILE="$SCRIPT_DIR/hosts/$SERVER_NAME/secrets.yaml"
 
 umask 0177
 cp "$KEYRING_PATH/sops_age_$SERVER_NAME.key" ./var/lib/sops
