08333372595612b6e5957cf6c8b7522d97babc3a
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-06-05 14:33:40 +0300

    refactor(terraform): Merge terraform files

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~>1.47.0"
+    }
+  }
+}
+
+variable "hcloud_token" {
+  sensitive = true
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
 locals {
   keyring_path = "/home/ldryt/.keyring"
 }
