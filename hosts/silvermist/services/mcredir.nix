98b7f30a06833b92ff3d9a5a92f5516f74e3aac0
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-07-09 19:37:11 +0300

    feat(hosts/silvermist): mcredir: Specify config file

--- hosts/silvermist/services/mcredir.nix
+++ hosts/silvermist/services/mcredir.nix
@@ -1,10 +1,22 @@
-{ flakePackages, ... }:
+{ pkgs, flakePackages, ... }:
+let
+  mcredirPort = 25565;
+  mcredirConfig = pkgs.writeText "mcredirConfig" ''
+    listen-address: "0.0.0.0:${toString mcredirPort}"
+
+    mc:
+      version: "1.21"
+      protocol: 767
+      motd: "Click here to start the server."
+  '';
+in
 {
+  networking.firewall.allowedTCPPorts = [ mcredirPort ];
   systemd.services.mcredir = {
     wants = [ "network-online.target" ];
     after = [ "network-online.target" ];
     serviceConfig = {
-      ExecStart = "${flakePackages.mcredir}/bin/mcredir";
+      ExecStart = "${flakePackages.mcredir}/bin/mcredir ${mcredirConfig}";
     };
   };
 }
