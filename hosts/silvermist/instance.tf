b948933938392b95187bdfdca6dee26e373e7274
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-07-09 23:44:34 +0300

    refactor(hosts/silvermist): Remove hardware firewall, rely on software firewall

--- hosts/silvermist/instance.tf
+++ hosts/silvermist/instance.tf
@@ -1,41 +1,3 @@
-resource "hcloud_firewall" "silvermist_firewall" {
-  labels = {
-    "silvermist" : true
-  }
-  name = "silvermist_firewall"
-  rule {
-    direction = "in"
-    protocol  = "icmp"
-    source_ips = [
-      "0.0.0.0/0"
-    ]
-  }
-  rule {
-    direction = "in"
-    protocol  = "tcp"
-    port      = "80"
-    source_ips = [
-      "0.0.0.0/0"
-    ]
-  }
-  rule {
-    direction = "in"
-    protocol  = "tcp"
-    port      = "443"
-    source_ips = [
-      "0.0.0.0/0"
-    ]
-  }
-  rule {
-    direction = "in"
-    protocol  = "tcp"
-    port      = "22"
-    source_ips = [
-      "0.0.0.0/0"
-    ]
-  }
-}
-
 resource "hcloud_primary_ip" "silvermist_ipv4" {
   labels = {
     "silvermist" : true
@@ -63,7 +25,6 @@ resource "hcloud_server" "silvermist_server" {
   image        = "debian-12"
   server_type  = "cx22"
   datacenter   = "fsn1-dc14"
-  firewall_ids = [hcloud_firewall.silvermist_firewall.id]
   ssh_keys     = [hcloud_ssh_key.silvermist_ssh_key.id]
   public_net {
     ipv4_enabled = true
