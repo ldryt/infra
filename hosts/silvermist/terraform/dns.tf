9b0d6b93eff672dfe7bf7b6b3e58ea2631e4b086
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-07-21 20:12:51 +0300

    fix(hosts/silvermist): terraform: Correct zone_id

--- hosts/silvermist/terraform/dns.tf
+++ hosts/silvermist/terraform/dns.tf
@@ -1,46 +1,50 @@
+locals {
+  zone_id = "39509d1b1fb62e1c6623562d537b0bcb"
+}
+
 resource "cloudflare_record" "silvermist_A_record" {
+  zone_id = local.zone_id
   name    = "silvermist"
-  zone_id = local.dns.zone
-  type    = "A"
   value   = hcloud_primary_ip.silvermist_ipv4.ip_address
+  type    = "A"
   proxied = false
 }
 
 resource "cloudflare_record" "root_A_record" {
+  zone_id = local.zone_id
   name    = "@"
-  zone_id = local.dns.zone
-  type    = "A"
   value   = hcloud_primary_ip.silvermist_ipv4.ip_address
+  type    = "A"
   proxied = false
 }
 
 resource "cloudflare_record" "silvermist_subdomains" {
   for_each = local.dns.subdomains
 
+  zone_id = local.zone_id
   name    = each.value
-  zone_id = local.dns.zone
-  type    = "CNAME"
   value   = "silvermist"
+  type    = "CNAME"
   proxied = false
 }
 
 resource "cloudflare_record" "root_MX_record" {
+  zone_id = local.zone_id
   name    = "@"
-  zone_id = local.dns.zone
-  type    = "MX"
   value   = local.dns.zone
+  type    = "MX"
 }
 
 resource "cloudflare_record" "root_TXT_record__SPF" {
+  zone_id = local.zone_id
   name    = "@"
-  zone_id = local.dns.zone
-  type    = "TXT"
   value   = "v=spf1 mx ~all"
+  type    = "TXT"
 }
 
 resource "cloudflare_record" "root_TXT_record__DKIM" {
+  zone_id = local.zone_id
   name    = "@"
-  zone_id = local.dns.zone
-  type    = "TXT"
   value   = nonsensitive(data.sops_file.silvermist_secrets.data["services.opendkim.selectors.main.txt"])
+  type    = "TXT"
 }
