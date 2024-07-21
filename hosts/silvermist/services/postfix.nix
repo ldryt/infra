5549d8e4a56cb17647814e4243662dff4123c514
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-07-21 17:34:45 +0300

    feat(hosts/silvermist): postfix: Add DKIM

--- hosts/silvermist/services/postfix.nix
+++ hosts/silvermist/services/postfix.nix
@@ -1,4 +1,4 @@
-{...}:
+{ config, lib, ... }:
 let
   dns = builtins.fromJSON (builtins.readFile ../dns.json);
 in
@@ -8,6 +8,30 @@ in
     hostname = dns.zone;
     config = {
       inet_interfaces = "loopback-only";
+
+      milter_protocol = "6";
+      milter_default_action = "accept";
+      smtpd_milters = "unix:${config.services.opendkim.socket}";
+      non_smtpd_milters = "unix:${config.services.opendkim.socket}";
     };
   };
+
+  sops.secrets."services/opendkim/selectors/main/private".owner = config.services.postfix.user;
+  services.opendkim = {
+    enable = true;
+    user = config.services.postfix.user;
+    group = config.services.postfix.group;
+    selector = "main";
+    domains = config.services.postfix.domain;
+    socket = "/run/opendkim/opendkim.sock";
+  };
+  systemd.services.opendkim.preStart = lib.mkForce ''
+    install \
+      -o ${config.services.postfix.user} \
+      -g ${config.services.postfix.group} \
+      -m0700 \
+      -D \
+      ${config.sops.secrets."services/opendkim/selectors/main/private".path} \
+      ${config.services.opendkim.keyPath}/${config.services.opendkim.selector}.private
+  '';
 }
--- hosts/silvermist/terraform/dns.tf
+++ hosts/silvermist/terraform/dns.tf
@@ -23,3 +23,27 @@ resource "gandi_livedns_record" "silvermist_subdomains" {
   values = ["silvermist"]
   ttl    = 86400
 }
+
+resource "gandi_livedns_record" "root_MX_record" {
+  name   = "@"
+  zone   = local.dns.zone
+  type   = "MX"
+  values = [local.dns.zone]
+  ttl    = 86400
+}
+
+resource "gandi_livedns_record" "root_TXT_record__SPF" {
+  name   = "@"
+  zone   = local.dns.zone
+  type   = "TXT"
+  values = ["v=spf1 mx ~all"]
+  ttl    = 86400
+}
+
+resource "gandi_livedns_record" "root_TXT_record__DKIM" {
+  name   = "@"
+  zone   = local.dns.zone
+  type   = "TXT"
+  values = [nonsensitive(data.sops_file.silvermist_secrets.data["services.opendkim.selectors.main.txt"])]
+  ttl    = 86400
+}
