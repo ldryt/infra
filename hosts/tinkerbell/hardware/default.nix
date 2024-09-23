d3a7ad79a5388260e72a94d803620f3374e14529
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-09-23 15:39:12 +0200

    fix(hosts/tinkerbell): hardware: Remove unused kernel modules

--- hosts/tinkerbell/hardware/default.nix
+++ hosts/tinkerbell/hardware/default.nix
@@ -4,12 +4,14 @@
   ...
 }:
 {
+  # Note: module "framework-13-7040-amd" from https://github.com/NixOS/nixos-hardware is imported in flake
+
   imports = [ ./disko.nix ];
 
   services.fwupd.enable = true;
 
   # https://nixos.org/manual/nixos/unstable/index.html#sec-gpu-accel-vulkan
-  hardware.opengl = {
+  hardware.graphics = {
     extraPackages = with pkgs; [
       amdvlk
     ];
@@ -19,32 +21,17 @@
   };
 
   boot = {
-    loader.systemd-boot.enable = false;
+    kernelParams = [ "quiet" ];
+
     loader.efi.canTouchEfiVariables = true;
+
+    # Lanzaboote currently replaces the systemd-boot module.
+    loader.systemd-boot.enable = false;
+    initrd.systemd.enable = true;
     lanzaboote = {
       enable = true;
       pkiBundle = "/etc/secureboot";
     };
-
-    initrd = {
-      systemd.enable = true;
-
-      availableKernelModules = [
-        "xhci_pci"
-        "ahci"
-        "nvme"
-        "usb_storage"
-        "sd_mod"
-        "rtsx_pci_sdmmc"
-      ];
-      kernelModules = [ "dm-snapshot" ];
-    };
-
-    kernelModules = [
-      "kvm-intel"
-      "btusb"
-    ];
-    kernelParams = [ "quiet" ];
   };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
