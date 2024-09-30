4a9a994cd355f971ee3c66bfcb0544c5d128f423
Lucas Ladreyt <lucas.ladreyt@epita.fr>
2024-09-30 10:58:07 +0200

    feat(users): vim: tabs as 4 spaces

--- users/commons/vim.nix
+++ users/commons/vim.nix
@@ -8,8 +8,15 @@
       gitgutter
     ];
     extraConfig = ''
+      filetype plugin indent on
+      " show existing tab with 4 spaces width
+      set tabstop=4
+      " when indenting with '>', use 4 spaces width
+      set shiftwidth=4
+      " On pressing tab, insert 4 spaces
+      set expandtab
+
       set nobackup   
-      set autoindent
       set number
       set colorcolumn=80
     '';
