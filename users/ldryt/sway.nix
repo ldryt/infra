1d04b712ef7c8fabfbf251f0e00abd76f93e861d
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-10-21 22:49:23 +0200

    refactor(users/ldryt): refactor sway

--- users/ldryt/sway.nix
+++ users/ldryt/sway.nix
@@ -1,19 +1,7 @@
-{ pkgs, ... }:
-let
-  swaylock-cfg = pkgs.writeText "swaylock-cfg" ''
-    daemonize
-    show-failed-attempts
-    ignore-empty-password
-    indicator-idle-visible
-    indicator-radius=150
-    color=000000
-    ring-color=ffffff
-    inside-ver-color=000000
-  '';
-  lock = "${pkgs.swaylock}/bin/swaylock --config=${swaylock-cfg}";
-in
+{ lib, config, pkgs, ... }:
 {
   imports = [
+    ./swaylock.nix
     ../commons/alacritty.nix
   ];
 
@@ -98,157 +86,53 @@ in
     };
   };
 
-  services = {
-    swayidle = {
-      enable = true;
-      events = [
-        {
-          event = "lock";
-          command = lock;
-        }
-        {
-          event = "before-sleep";
-          command = lock;
-        }
-      ];
-    };
-    wlsunset = {
-      enable = true;
-      sunrise = "07:00";
-      sunset = "20:00";
-    };
+  services.wlsunset = {
+    enable = true;
+    sunrise = "07:00";
+    sunset = "20:00";
   };
 
   wayland.windowManager.sway = {
     enable = true;
     checkConfig = true;
-    config = null; # disable default config
-    extraConfig = ''
-      # set modifier (super key)
-      set $mod Mod4
+    config = {
+      modifier = "Mod4";
+      terminal = "alacritty";
+      window = {
+        hideEdgeBorders = "both";
+        border = 1;
+      };
+      keybindings = let
+        mod = config.wayland.windowManager.sway.config.modifier;
+      in lib.mkOptionDefault {
+        "${mod}+d" = "exec ${pkgs.dmenu-wayland}/bin/dmenu-wl_run"; 
 
-      # set font
-      font pango:monospace 11
+        "${mod}+f" = "fullscreen";
 
-      # custom borders
-      default_border pixel 1
+        "XF86AudioMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+        "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_SINK@ 5%-";
+        "XF86AudioRaiseVolume" = "exec wpctl set-volume @DEFAULT_SINK@ 5%+";
 
-      # lock the screen
-      bindsym $mod+l exec ${lock}
+        "XF86MonBrightnessDown" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%-";
+        "XF86MonBrightnessUp" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%+";
 
-      # start alacritty
-      bindsym $mod+Return exec alacritty
-
-      # kill focused window
-      bindsym $mod+Shift+q kill
-
-      # program launcher
-      bindsym $mod+d exec ${pkgs.dmenu-wayland}/bin/dmenu-wl_run
-
-      # change focus
-      bindsym $mod+Left focus left
-      bindsym $mod+Down focus down
-      bindsym $mod+Up focus up
-      bindsym $mod+Right focus right
-
-      # move focused window
-      bindsym $mod+Shift+Left move left
-      bindsym $mod+Shift+Down move down
-      bindsym $mod+Shift+Up move up
-      bindsym $mod+Shift+Right move right
-
-      # split in horizontal orientation
-      bindsym $mod+c split h
-
-      # split in vertical orientation
-      bindsym $mod+v split v
-
-      # enter fullscreen mode for the focused container
-      bindsym $mod+f fullscreen
-
-      # change container layout (stacked, tabbed, toggle split)
-      bindsym $mod+s layout stacking
-      bindsym $mod+w layout tabbed
-      bindsym $mod+e layout toggle split
-
-      # toggle tiling / floating
-      bindsym $mod+Shift+space floating toggle
-
-      # change focus between tiling / floating windows
-      bindsym $mod+space focus mode_toggle
-
-      # focus the parent container
-      bindsym $mod+a focus parent
-
-      # focus the child container
-      #bindsym $mod+d focus child
-
-      # switch to workspace
-      bindsym $mod+1 workspace 1
-      bindsym $mod+2 workspace 2
-      bindsym $mod+3 workspace 3
-      bindsym $mod+4 workspace 4
-      bindsym $mod+5 workspace 5
-      bindsym $mod+6 workspace 6
-      bindsym $mod+7 workspace 7
-      bindsym $mod+8 workspace 8
-      bindsym $mod+9 workspace 9
-
-      # move focused container to workspace
-      bindsym $mod+Shift+1 move container to workspace 1
-      bindsym $mod+Shift+2 move container to workspace 2
-      bindsym $mod+Shift+3 move container to workspace 3
-      bindsym $mod+Shift+4 move container to workspace 4
-      bindsym $mod+Shift+5 move container to workspace 5
-      bindsym $mod+Shift+6 move container to workspace 6
-      bindsym $mod+Shift+7 move container to workspace 7
-      bindsym $mod+Shift+8 move container to workspace 8
-      bindsym $mod+Shift+9 move container to workspace 9
-
-
-      # reload the configuration file
-      bindsym $mod+Shift+c reload
-      # restart inplace
-      bindsym $mod+Shift+r restart
-
-      # adjust volume via pulseaudio
-      bindsym --locked XF86AudioMute exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-      bindsym --locked XF86AudioLowerVolume exec wpctl set-volume @DEFAULT_SINK@ 5%-
-      bindsym --locked XF86AudioRaiseVolume exec wpctl set-volume @DEFAULT_SINK@ 5%+
-
-      # adjust brightness via brightnessctl
-      bindsym --locked XF86MonBrightnessDown exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%-
-      bindsym --locked XF86MonBrightnessUp exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%+
-
-      # take a screenshot to clipboard
-      bindsym Print exec grim -g "$(slurp)" - | wl-copy
-
-      # set wallpaper using swaybg
-      output "*" bg ${../commons/wallpaper.jpg} fill
-
-      # HiDPi scaling for internal display
-      output "eDP-1" scale 1.3
-      output "eDP-1" scale_filter smart
-
-      # touchpad tap-to-click and natural scrolling
-      input type:touchpad {
-        tap enabled
-        natural_scroll enabled
-      }
-
-      # share clipboard content amongst windows
-      exec wl-paste -t text --watch clipman store --no-persist
-
-      # tell at a glance which windows are using Xwayland
-      for_window [shell="xwayland"] title_format "[XWayland] %title"
-
-      ## framework 13 color profile
-      # output "*" color_profile icc ${./fw13.icm}
-
-      bar {
-        position bottom
-        status_command i3status
-      }
-    '';
+        "Print" = "exec grim -g \"$(slurp)\" - | wl-copy";
+      };
+      input = {
+        "type:touchpad" = {
+          tap = "enabled";
+          natural_scroll = "enabled";
+        };
+      };
+      output = {
+        "*" = {
+          bg = "${../commons/wallpaper.jpg} fill";
+        };
+        "eDP-1" = {
+          scale = "1.3";
+          scale_filter = "smart";
+        };
+      };
+    };
   };
 }
