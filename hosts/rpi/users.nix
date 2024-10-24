5268c65fa6b36d8a7022ce35e79b29d05836dbf3
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-10-25 00:34:24 +0200

    fix(hosts/rpi): users: ldryt: fix openssh authorized key

--- hosts/rpi/users.nix
+++ hosts/rpi/users.nix
@@ -10,7 +10,7 @@
       ];
       hashedPasswordFile = config.sops.secrets."users/ldryt/hashedPassword".path;
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII15TWK7X30dmgSO3izk1NFiMB6LAAWztoEAx2qKC/X7"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOJwYm0rvWGewXyk/KwCCSLm4yv5t57zi/+XBz+ugcYE"
       ];
     };
   };
