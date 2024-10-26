f1fc6e643d331bec0b46e4fe89cc6b02e787447b
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-10-26 10:09:39 +0200

    fix(hosts/rpi): Correct user name

--- hosts/rpi/users.nix
+++ hosts/rpi/users.nix
@@ -1,18 +1,18 @@
 { config, ... }:
 {
-  sops.secrets."users/ldryt/hashedPassword".neededForUsers = true;
+  sops.secrets."users/colon/hashedPassword".neededForUsers = true;
   users = {
     mutableUsers = false;
-    users.ldryt = {
+    users.colon = {
       isNormalUser = true;
       extraGroups = [
         "wheel"
       ];
-      hashedPasswordFile = config.sops.secrets."users/ldryt/hashedPassword".path;
+      hashedPasswordFile = config.sops.secrets."users/colon/hashedPassword".path;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOJwYm0rvWGewXyk/KwCCSLm4yv5t57zi/+XBz+ugcYE"
       ];
     };
   };
-  nix.settings.trusted-users = [ config.users.users.ldryt.name ];
+  nix.settings.trusted-users = [ config.users.users.colon.name ];
 }
