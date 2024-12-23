4762c4750c316a73320d4e70c9ccdab441213d62
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-12-23 14:50:53 +0100

    feat(modules/nix-settings): Improve gc

--- modules/nix-settings.nix
+++ modules/nix-settings.nix
@@ -6,14 +6,16 @@
         "nix-command"
         "flakes"
       ];
-      auto-optimise-store = true;
       substituters = [ "https://nix-community.cachix.org" ];
       trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
     };
+
+    optimise.automatic = true;
+
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 7d";
+      options = "--delete-older-than 30d";
     };
   };
 }
