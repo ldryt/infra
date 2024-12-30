451f8a9770b41c19c3e3dee3392546c2868e27f8
Lucas Ladreyt <git@ldryt.anonaddy.me>
2024-12-30 18:17:10 +0100

    fix(modules/chrony): In case of DNS failure

--- modules/chrony.nix
+++ modules/chrony.nix
@@ -6,8 +6,14 @@
     servers = [
       "nts.netnod.se"
       "nts.teambelgium.net"
-      "time.cloudflare.com"
       "ntp.3eck.net"
+      "time.cloudflare.com"
+      "time.google.com"
+
+      # In case of DNS failure
+      "162.159.200.123" # time.cloudflare.com
+      "94.224.65.118"   # nts.teambelgium.net
+      "216.239.35.4"    # time.google.com
     ];
     extraConfig = ''
       makestep 1 -1
