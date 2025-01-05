06819a15c0ee819973a0569477b7dc3491d62494
Lucas Ladreyt <git@ldryt.anonaddy.me>
2025-01-05 04:43:53 +0100

    refactor(modules/dns): switch to dnscrypt-proxy and avahi

--- modules/dns.nix
+++ modules/dns.nix
@@ -1,20 +1,35 @@
 { ... }:
 {
-  networking.nameservers = [
-    "9.9.9.11#dns.quad9.net"
-    "149.112.112.112#dns.quad9.net"
-    "2620:fe::fe#dns.quad9.net"
-    "2620:fe::9#dns.quad9.net"
-  ];
+  networking.nameservers = [ "127.0.0.53" ];
 
-  services.resolved = {
+  services.dnscrypt-proxy2 = {
     enable = true;
-    llmnr = "false";
-    dnsovertls = "opportunistic";
-    domains = [ "~." ];
-    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
-    extraConfig = ''
-      MulticastDNS=yes
-    '';
+    settings = {
+      log_level = 0;
+      listen_addresses = [ "127.0.0.53:53" ];
+      require_dnssec = true;
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/lib/dnscrypt-proxy/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        refresh_delay = 72;
+      };
+    };
+  };
+
+  # No nameservers overrides
+  networking.networkmanager.dns = "none";
+  networking.dhcpcd.extraConfig = "nohook resolv.conf";
+  services.resolved.enable = false;
+
+  # Enable mDNS resolving
+  services.avahi = {
+    enable = true;
+    ipv6 = true;
+    nssmdns6 = true;
+    nssmdns4 = true;
   };
 }
