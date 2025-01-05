1e40001bd170600dac144061d3491afbff34629f
Lucas Ladreyt <git@ldryt.anonaddy.me>
2025-01-05 05:04:39 +0100

    \!fixed secure dns and ntp without rtc combo by disabling nts and adding ntp server ip addresses

--- modules/chrony.nix
+++ modules/chrony.nix
@@ -2,12 +2,15 @@
 {
   services.chrony = {
     enable = true;
-    enableNTS = true;
     servers = [
-      "nts.netnod.se"
-      "nts.teambelgium.net"
+      "132.163.96.1"
+      "132.163.97.1"
+      "104.16.133.229"
+      "194.58.200.20"
+      "ntp.se"
       "ntp.3eck.net"
       "time.cloudflare.com"
+      "time.nist.gov"
     ];
   };
 }
