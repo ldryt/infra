fa87ebca396eb57812f0071b25be030224caf646
Lucas Ladreyt <git@ldryt.anonaddy.me>
2025-01-13 23:24:38 +0100

    fix(dns): switch to plain 9.9.9.9

--- modules/dns.nix
+++ modules/dns.nix
@@ -1,24 +1,6 @@
 { ... }:
 {
-  networking.nameservers = [ "127.0.0.53" ];
-
-  services.dnscrypt-proxy2 = {
-    enable = true;
-    settings = {
-      log_level = 0;
-      listen_addresses = [ "127.0.0.53:53" ];
-      require_dnssec = true;
-      sources.public-resolvers = {
-        urls = [
-          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-        ];
-        cache_file = "/var/lib/dnscrypt-proxy/public-resolvers.md";
-        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-        refresh_delay = 72;
-      };
-    };
-  };
+  networking.nameservers = [ "9.9.9.9" ];
 
   # No nameservers overrides
   networking.networkmanager.dns = "none";
@@ -28,8 +10,11 @@
   # Enable mDNS resolving
   services.avahi = {
     enable = true;
-    ipv6 = true;
-    nssmdns6 = true;
+    openFirewall = true;
     nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+    };
   };
 }
