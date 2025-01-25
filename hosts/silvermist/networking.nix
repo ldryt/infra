98e8f49648bd0e09dd72932ca14f67b74d1ecd1a
Lucas Ladreyt <git@ldryt.anonaddy.me>
2025-01-25 12:29:35 +0100

    refactor(silvermist): simplified networking

--- hosts/silvermist/networking.nix
+++ hosts/silvermist/networking.nix
@@ -3,12 +3,7 @@
   networking = {
     hostName = "silvermist";
     useNetworkd = true;
+    useDHCP = true;
     enableIPv6 = false;
-    usePredictableInterfaceNames = false; # just one eth0
-  };
-
-  systemd.network.networks."10-eth0" = {
-    matchConfig.Name = "eth0";
-    networkConfig.DHCP = "ipv4";
   };
 }
