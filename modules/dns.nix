032b04b02f8b2f22d356d06a41c0390aba1d8b78
Lucas Ladreyt <git@ldryt.anonaddy.me>
2025-01-28 23:52:04 +0100

    feat(dns): more dns servers

--- modules/dns.nix
+++ modules/dns.nix
@@ -1,6 +1,11 @@
 { ... }:
 {
-  networking.nameservers = [ "9.9.9.9" ];
+  networking.nameservers = [
+    "9.9.9.9"
+    "149.112.112.112"
+    "2620:fe::fe"
+    "2620:fe::9"
+  ];
 
   # No nameservers overrides
   networking.networkmanager.dns = "none";
