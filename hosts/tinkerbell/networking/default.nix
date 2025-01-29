6bfb4097cdb0f47e2f35081f59967fac513e44da
Lucas Ladreyt <git@ldryt.anonaddy.me>
2025-01-29 22:34:02 +0100

    fix(tinkerbell): remove iwd

--- hosts/tinkerbell/networking/default.nix
+++ hosts/tinkerbell/networking/default.nix
@@ -5,13 +5,11 @@
     ../../../modules/dns.nix
     ../../../modules/chrony.nix
   ];
-  environment.systemPackages = [ pkgs.iwd ]; # for iwctl
   networking = {
     hostName = "tinkerbell";
     networkmanager = {
       enable = true;
       wifi = {
-        backend = "iwd";
         powersave = true;
         macAddress = "stable-ssid";
       };
