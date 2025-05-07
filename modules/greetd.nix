b10a0a23761e7ad3cc54d0a0716c2962e67b74c1
Lucas Ladreyt <git@ldryt.anonaddy.me>
2025-05-07 13:22:18 +0200

    refactor(modules/greetd): use tty2

--- modules/greetd.nix
+++ modules/greetd.nix
@@ -2,6 +2,7 @@
 {
   services.greetd = {
     enable = true;
+    vt = 2;
     settings = {
       default_session.command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${pkgs.sway}/bin/sway";
     };
