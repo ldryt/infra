8f3f2d2e5845efd1ed3958c4b88f49ee08586fd9
Lucas Ladreyt <git@ldryt.anonaddy.me>
2025-07-25 20:39:23 +0300

    fix(hosts/tp420ia): sftp-backups: typo in authorizedKeys

--- hosts/tp420ia/services/sftp-backups.nix
+++ hosts/tp420ia/services/sftp-backups.nix
@@ -18,11 +18,11 @@ in
     isSystemUser = true;
     group = bckpUser;
     uid = 1442;
-    openssh.authorizedKeys = ''
-      ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEqS0CzQnK6xfd/dch7Jm1QQCYZUuZyWgXZYImQ8OmiD colon@domus
-      ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE6aepmmBNvNShGIp6gJM+TcXej/SIQokttO6ArjfTcv colon@silvermist
-      ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGb0ZhjF+uNHovVeddQ7ZikoMl3DuQmajM3o6QYx3inm ldryt@tinkerbell
-    '';
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEqS0CzQnK6xfd/dch7Jm1QQCYZUuZyWgXZYImQ8OmiD colon@domus"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE6aepmmBNvNShGIp6gJM+TcXej/SIQokttO6ArjfTcv colon@silvermist"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGb0ZhjF+uNHovVeddQ7ZikoMl3DuQmajM3o6QYx3inm ldryt@tinkerbell"
+    ];
   };
 
   services.openssh.extraConfig = ''
