6817b25cd0ffff5e4f5b4d94eac9f3dd795f82f0
Lucas Ladreyt <git@ldryt.anonaddy.me>
2025-07-27 17:03:20 +0300

    feat(users/common): packages: more monitoring tools

--- users/common/packages/cli.nix
+++ users/common/packages/cli.nix
@@ -1,9 +1,15 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    atop
     btop
     iotop
     iftop
+    nvtopPackages.amd
+    linuxPackages_latest.perf
+    wavemon
+
+    screen
     nmap
     tcpdump
     iperf3
