f659a0dc4208dcbee1a40f7b8ea1e4298c87cec6
Lucas Ladreyt <ldryt@posteo.com>
2025-07-30 12:14:29 +0300

    fix(hosts/tp420ia): networking: specify route metrics to address rp_filter/routing bugs

--- hosts/tp420ia/networking/nmprofiles.nix
+++ hosts/tp420ia/networking/nmprofiles.nix
@@ -4,6 +4,28 @@
   networking.networkmanager.ensureProfiles = {
     environmentFiles = [ config.sops.secrets."system/NetworkManager/profiles/env".path ];
     profiles = {
+      enp3s0f4u1i4 = {
+        connection = {
+          id = "enp3s0f4u1i4";
+          interface-name = "enp3s0f4u1i4";
+          type = "802-3-ethernet";
+        };
+        ipv4 = {
+          method = "auto";
+          route-metric = 200;
+        };
+        ipv6 = {
+          method = "disabled";
+        };
+      };
+      enp3s0f3u1u2 = {
+        connection = {
+          id = "enp3s0f3u1u2";
+          type = "802-3-ethernet";
+          interface-name = "enp3s0f3u1u2";
+          autoconnect = "no";
+        };
+      };
       GNB = {
         connection = {
           id = "$GNB_SSID";
@@ -16,6 +38,13 @@
         wifi-security = {
           key-mgmt = "wpa-psk";
           psk = "$GNB_PWD";
+        };
+        ipv4 = {
+          method = "auto";
+          route-metric = 20;
+        };
+        ipv6 = {
+          method = "disabled";
         };
       };
       rosetta = {
@@ -30,6 +59,13 @@
         wifi-security = {
           key-mgmt = "wpa-psk";
           psk = "$ROSETTA_PWD";
+        };
+        ipv4 = {
+          method = "auto";
+          route-metric = 10;
+        };
+        ipv6 = {
+          method = "disabled";
         };
       };
     };
