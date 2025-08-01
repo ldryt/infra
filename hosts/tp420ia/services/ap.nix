d436ff0cf26565de9efcca7563db96906d02137e
Lucas Ladreyt <ldryt@posteo.com>
2025-08-01 02:27:30 +0200

    fix(hosts/tp420ia): rollback to one ap (netdevs unstable mt7922)

--- hosts/tp420ia/services/ap.nix
+++ hosts/tp420ia/services/ap.nix
@@ -1,83 +1,48 @@
 { config, ... }:
 let
   ssid = "tp420ia";
-  r24 = {
-    intf = "ap24";
-    addr = "192.168.24.0/24";
-  };
-  r5 = {
-    intf = "ap5";
-    addr = "192.168.5.0/24";
-  };
+  mac = "58:cd:c9:3a:78:1b"; # mt7922
+  intf = "ap0";
+  ip = "192.168.144.1";
+  mask = "24";
 in
 {
   systemd.network = {
-    netdevs = {
-      "10-${r24.intf}" = {
-        netdevConfig = {
-          Kind = "wlan";
-          Name = r24.intf;
-          MACAddress = "ba:ba:ba:ba:ba:24";
-        };
-        wlanConfig = {
-          PhysicalDevice = 0;
-          Type = "ap";
-        };
-      };
-      "10-${r5.intf}" = {
-        netdevConfig = {
-          Kind = "wlan";
-          Name = r5.intf;
-          MACAddress = "ba:ba:ba:ba:ba:05";
-        };
-        wlanConfig = {
-          PhysicalDevice = 0;
-          Type = "ap";
-        };
+    links = {
+      "10-${intf}" = {
+        matchConfig.PermanentMACAddress = mac;
+        linkConfig.Name = intf;
       };
     };
-    networks =
-      let
+    networks = {
+      "10-${intf}" = {
+        matchConfig.Name = intf;
+        networkConfig = {
+          Address = ip + "/" + mask;
+          DHCPServer = "yes";
+          IPMasquerade = "ipv4";
+          IPv4Forwarding = "yes";
+        };
         dhcpServerConfig = {
           PoolOffset = 10;
           PoolSize = 200;
           EmitDNS = "yes";
           DNS = "9.9.9.9";
         };
-      in
-      {
-        "10-${r24.intf}" = {
-          matchConfig.Name = r24.intf;
-          networkConfig = {
-            Address = r24.addr;
-            DHCPServer = "yes";
-            IPMasquerade = "ipv4";
-            IPv4Forwarding = "yes";
-          };
-          inherit dhcpServerConfig;
-        };
-        "10-${r5.intf}" = {
-          matchConfig.Name = r5.intf;
-          networkConfig = {
-            Address = r5.addr;
-            DHCPServer = "yes";
-            IPMasquerade = "ipv4";
-            IPv4Forwarding = "yes";
-          };
-          inherit dhcpServerConfig;
-        };
       };
+    };
   };
 
-  networking.firewall.interfaces."${r24.intf}".allowedUDPPorts = [ 67 ];
-  networking.firewall.interfaces."${r5.intf}".allowedUDPPorts = [ 67 ];
+  networking.firewall.interfaces."${intf}".allowedUDPPorts = [ 67 ];
 
   sops.secrets."services/hostapd/password" = { };
   services.hostapd = {
     enable = true;
-    radios =
-      let
+    radios = {
+      "${intf}" = {
         countryCode = "FR";
+        band = "2g";
+        channel = 1;
         wifi4 = {
           enable = true;
           capabilities = [
@@ -88,7 +53,7 @@ in
             "SHORT-GI-40"
           ];
         };
-        network = {
+        networks."${intf}" = {
           logLevel = 1;
           inherit ssid;
           authentication = {
@@ -98,27 +63,7 @@ in
             enableRecommendedPairwiseCiphers = true;
           };
         };
-      in
-      {
-        "${r24.intf}" = {
-          inherit wifi4 countryCode;
-          band = "2g";
-          channel = 11;
-          networks."${r24.intf}" = network;
-        };
-        "${r5.intf}" = {
-          inherit wifi4 countryCode;
-          band = "5g";
-          channel = 36;
-          wifi5 = {
-            enable = true;
-            operatingChannelWidth = "160";
-            capabilities = [
-              "SHORT-GI-80"
-            ];
-          };
-          networks."${r5.intf}" = network;
-        };
       };
+    };
   };
 }
