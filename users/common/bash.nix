6c78246b0c7479e9b779b2b3ce4edde5ddbd5240
Lucas Ladreyt <ldryt@posteo.com>
2025-12-07 10:54:51 +0100

    feat(users): bash: PS1: add nix-shell indicator

--- users/common/bash.nix
+++ users/common/bash.nix
@@ -4,13 +4,21 @@
   programs.bash = {
     enableCompletion = true;
     bashrcExtra = ''
-      export PS1='$(git branch &>/dev/null; if [ $? -eq 0 ]; then \
-      echo "\[\e[1m\]\u@\h\[\e[0m\]: \W [\[\e[34m\]$(git branch \
-        | grep ^* | sed s/\*\ //)\[\e[0m\]\
-      $(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; \
-        if [ "$?" -ne "0" ]; then \
-      echo "\[\e[1;31m\]*\[\e[0m\]"; fi)] \$ "; else \
-      echo "\[\e[1m\]\u@\h\[\e[0m\]: \W \$ "; fi )'
+      export PS1='$(
+        if [ -n "$IN_NIX_SHELL" ]; then
+            echo -n "\[\e[1;36m\](nix)\[\e[0m\] ";
+        fi;
+        git branch &>/dev/null;
+        if [ $? -eq 0 ]; then
+            echo "\[\e[1m\]\u@\h\[\e[0m\]: \W [\[\e[34m\]$(git branch \
+            | grep ^* | sed s/\*\ //)\[\e[0m\]\
+            $(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; \
+            if [ "$?" -ne "0" ]; then \
+            echo "\[\e[1;31m\]*\[\e[0m\]"; fi)] \$ ";
+        else
+            echo "\[\e[1m\]\u@\h\[\e[0m\]: \W \$ ";
+        fi
+      )'
     '';
   };
 }
