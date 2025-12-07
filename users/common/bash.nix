0130d00d6dbd96bc9816153d56f3bb1a96e55df0
Lucas Ladreyt <ldryt@posteo.com>
2025-12-07 10:56:39 +0100

    fix(users): bash: PS1: less latency for git indicator on big repos

--- users/common/bash.nix
+++ users/common/bash.nix
@@ -8,15 +8,17 @@
         if [ -n "$IN_NIX_SHELL" ]; then
             echo -n "\[\e[1;36m\](nix)\[\e[0m\] ";
         fi;
-        git branch &>/dev/null;
-        if [ $? -eq 0 ]; then
-            echo "\[\e[1m\]\u@\h\[\e[0m\]: \W [\[\e[34m\]$(git branch \
-            | grep ^* | sed s/\*\ //)\[\e[0m\]\
-            $(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; \
-            if [ "$?" -ne "0" ]; then \
-            echo "\[\e[1;31m\]*\[\e[0m\]"; fi)] \$ ";
+        if git rev-parse --is-inside-work-tree &>/dev/null; then
+            BRANCH=$(git symbolic-ref --short HEAD 2>/dev/null || git rev-parse --short HEAD 2>/dev/null)
+            if [ -n "$(git status --porcelain -uno 2>/dev/null)" ]; then
+                DIRTY="\[\e[1;31m\]*\[\e[0m\]"
+            else
+                DIRTY=""
+            fi
+
+            echo "\[\e[1m\]\u@\h\[\e[0m\]: \W [\[\e[34m\]$BRANCH\[\e[0m\]$DIRTY] \$ "
         else
-            echo "\[\e[1m\]\u@\h\[\e[0m\]: \W \$ ";
+            echo "\[\e[1m\]\u@\h\[\e[0m\]: \W \$ "
         fi
       )'
     '';
