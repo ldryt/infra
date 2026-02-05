1b75fc7df7c8448a3ebc963f2ba088dc2e3e33ce
Lucas Ladreyt <ldryt@posteo.com>
2026-02-05 22:58:04 +0100

    improve nix cache

--- hosts/luke/services/nix-cache.nix
+++ hosts/luke/services/nix-cache.nix
@@ -11,20 +11,56 @@ in
   sops.secrets."services/nix-cache/priv-key" = { };
   services.nix-serve = {
     enable = true;
+    package = pkgs.nix-serve-ng;
     bindAddress = "127.0.0.1";
     secretKeyFile = config.sops.secrets."services/nix-cache/priv-key".path;
   };
-  services.nginx.virtualHosts."${config.ldryt-infra.dns.records.nix-cache}" = {
-    forceSSL = true;
-    enableACME = true;
-    kTLS = true;
-    extraConfig = ''
-      client_max_body_size 0;
-      proxy_read_timeout 900s;
-    '';
-    locations."/".proxyPass =
-      "http://${config.services.nix-serve.bindAddress}:${toString config.services.nix-serve.port}";
-  };
+  services.nginx =
+    let
+      nixUpstream = "nix-serve";
+    in
+    {
+      upstreams."${nixUpstream}" = {
+        servers = {
+          "${config.services.nix-serve.bindAddress}:${toString config.services.nix-serve.port}" = { };
+        };
+        extraConfig = ''
+          keepalive 128;
+        '';
+      };
+      appendHttpConfig = ''
+        proxy_cache_path /var/cache/nginx/nix_cache levels=1:2 keys_zone=nix_cache:512m max_size=5g inactive=60m use_temp_path=off;
+      '';
+      virtualHosts."${config.ldryt-infra.dns.records.nix-cache}" = {
+        forceSSL = true;
+        enableACME = true;
+        kTLS = true;
+        extraConfig = ''
+          client_max_body_size 0;
+          proxy_read_timeout 30m;
+          proxy_buffering on;
+          proxy_buffer_size 16k;
+          proxy_buffers 32 16k;
+        '';
+        locations = {
+          "/" = {
+            proxyPass = "http://${nixUpstream}";
+            extraConfig = ''
+              proxy_set_header Connection "";
+            '';
+          };
+          "~ \\.narinfo$" = {
+            proxyPass = "http://${nixUpstream}";
+            extraConfig = ''
+              proxy_cache nix_cache;
+              proxy_cache_valid 200 45m;
+              proxy_cache_valid 404 15m;
+              proxy_set_header Connection "";
+            '';
+          };
+        };
+      };
+    };
   users.groups."${cacheUser}" = { };
   users.users."${cacheUser}" = {
     isSystemUser = true;
