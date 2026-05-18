b15e69f586884e1756e1e7fdb25f94cf922c54d1
Lucas Ladreyt <ldryt@posteo.com>
2026-05-18 15:45:45 +0200

    feat(hosts/tinkerbell): hibernate on low power

--- hosts/tinkerbell/hardware/power.nix
+++ hosts/tinkerbell/hardware/power.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.power-profiles-daemon.enable = true;
   powerManagement.powertop.enable = true;
@@ -11,6 +11,26 @@
   };
 
   systemd.sleep.extraConfig = ''
-    HibernateDelaySec=45m
+    HibernateDelaySec=30m
   '';
+
+  systemd.services.low-battery-hibernate = {
+    description = "Hibernate on low battery";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = pkgs.writeShellScript "low-battery-check" ''
+        capacity=$(cat /sys/class/power_supply/BAT1/capacity)
+        status=$(cat /sys/class/power_supply/BAT1/status)
+        if [ "$status" = "Discharging" ] && [ "$capacity" -le 5 ]; then
+          systemctl hibernate
+        fi
+      '';
+    };
+  };
+
+  systemd.timers.low-battery-hibernate = {
+    description = "Check battery level every minute";
+    wantedBy = [ "timers.target" ];
+    timerConfig.OnUnitActiveSec = "1min";
+  };
 }
