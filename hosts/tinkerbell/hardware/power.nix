f7cc97bbf4713248fc0854cbe57987e19dc94131
Lucas Ladreyt <ldryt@posteo.com>
2026-06-25 17:12:42 +0200

    fix(hosts/tinkerbell): power: use upower instead of broken udev rule

--- hosts/tinkerbell/hardware/power.nix
+++ hosts/tinkerbell/hardware/power.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 {
   services.power-profiles-daemon.enable = true;
 
@@ -13,8 +13,15 @@
     HibernateDelaySec=30m
   '';
 
+  services.upower = {
+    enable = true;
+    percentageLow = 20;
+    percentageCritical = 15;
+    percentageAction = 10;
+    criticalPowerAction = "Hibernate";
+  };
+
   services.udev.extraRules = ''
-    SUBSYSTEM=="power_supply", ATTR{type}=="Battery", ATTR{status}=="Discharging", ATTR{capacity}=="[0-10]", RUN+="${config.systemd.package}/bin/systemctl hibernate"
     SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver"
     SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set performance"
   '';
