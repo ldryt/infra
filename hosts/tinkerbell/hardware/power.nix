8f9512bc9b4fb5fc8a8642a5d200b1e8888f3edf
Lucas Ladreyt <ldryt@posteo.com>
2026-06-25 16:54:37 +0200

    fix(hosts/tinkerbell): hardware: udev attributes

--- hosts/tinkerbell/hardware/power.nix
+++ hosts/tinkerbell/hardware/power.nix
@@ -14,8 +14,8 @@
   '';
 
   services.udev.extraRules = ''
-    SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", ATTR{capacity}=="[0-5]", RUN+="${config.systemd.package}/bin/systemctl hibernate"
-    SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver"
-    SUBSYSTEM=="power_supply", ATTR{status}=="Charging", RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set performance"
+    SUBSYSTEM=="power_supply", ATTR{type}=="Battery", ATTR{status}=="Discharging", ATTR{capacity}=="[0-10]", RUN+="${config.systemd.package}/bin/systemctl hibernate"
+    SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver"
+    SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set performance"
   '';
 }
