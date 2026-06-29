157d3451d41e919d3196cf1859836ffef0ceff06
Lucas Ladreyt <ldryt@posteo.com>
2026-06-29 17:41:04 +0200

    chore(domus): ssh key rotation

--- hosts/domus/users.nix
+++ hosts/domus/users.nix
@@ -3,7 +3,7 @@
   ldryt-infra.users.colon = {
     enable = true;
     authorizedKeys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICg6pfvS/wc+6M2JgGuH5TdYE71S7aOsUn2IjyrR5RbH terraform@infra"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDkuAH3XI/M3jqbK+TNrWuG9/9w565xxICD6CJeuOqrK terraform@infra"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHlbB0gz194Jq9LSwI2OvsLcA+LgIQMWS2dNNhapaA8K ldryt@tinkerbell"
     ];
   };
